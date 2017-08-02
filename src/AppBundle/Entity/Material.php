902e5c81377a2e8137374ce914e3b164313f5884
Ewa Sek <ewasek@gmail.com>
2017-08-02 14:05:53 +0200

    Adding entities, controllers, sql

---
+++ src/AppBundle/Entity/Material.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Material
+ *
+ * @ORM\Table(name="material")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MaterialRepository")
+ */
+class Material
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255, unique=true)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Material
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Material
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ src/AppBundle/Entity/MaterialGroup.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MaterialGroup
+ *
+ * @ORM\Table(name="material_group")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MaterialGroupRepository")
+ */
+class MaterialGroup
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="right_side", type="integer")
+     */
+    private $rightSide;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="left_side", type="integer")
+     */
+    private $leftSide;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return MaterialGroup
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set rightSide
+     *
+     * @param integer $rightSide
+     * @return MaterialGroup
+     */
+    public function setRightSide($rightSide)
+    {
+        $this->rightSide = $rightSide;
+
+        return $this;
+    }
+
+    /**
+     * Get rightSide
+     *
+     * @return integer 
+     */
+    public function getRightSide()
+    {
+        return $this->rightSide;
+    }
+
+    /**
+     * Set leftSide
+     *
+     * @param integer $leftSide
+     * @return MaterialGroup
+     */
+    public function setLeftSide($leftSide)
+    {
+        $this->leftSide = $leftSide;
+
+        return $this;
+    }
+
+    /**
+     * Get leftSide
+     *
+     * @return integer 
+     */
+    public function getLeftSide()
+    {
+        return $this->leftSide;
+    }
+}
